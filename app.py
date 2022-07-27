bf49220f41503599e5644e0655283268d26638d2
Tejeswinath <101463250+Tejeswinath@users.noreply.github.com>
2022-07-27 07:08:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pickle
 import requests
 import pandas as pd
-data= pickle.load(open("data.pkl",'rb'))
-model=pickle.load(open("model.pkl",'rb'))
+data= pickle.load(open('data.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
 
 st.title("Air Quality Forecasting")
 
